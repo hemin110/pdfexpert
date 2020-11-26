28855081a6c44c613e21c37be64ee1fd75d28399
hemin <383974457@qq.com>
2020-11-26 23:01:18 +0800

    init

---
+++ src/main/java/com/xdata/pdfexpert/PdfexpertApplication.java
@@ -0,0 +1,13 @@
+package com.xdata.pdfexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfexpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfexpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xdata/pdfexpert/PdfexpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdata.pdfexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfexpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
